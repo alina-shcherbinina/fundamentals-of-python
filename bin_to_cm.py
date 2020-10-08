7e8499b3c2c3a61175f70d5944d1dd0d38236e51
Алина Щербинина <48168698+alina-shcherbinina@users.noreply.github.com>
2020-10-08 14:38:01 +0800

    fixed mistakes

--- bin_to_cm.py
+++ bin_to_cm.py
@@ -7,12 +7,15 @@ max_mm = float(text_file.readline()) # maximum mm first line
 lines = text_file.readlines()
 
 for i in range(1, len(lines)):
-    first = lines[i].split('\n')
+    first = lines[i].split()
+    # print(first)
     array.extend(first)
 
-line = array[0] # width of an image in px
-line = line.split() # split the other lines 
-pixel = len(line) # length
- 
-Resolution = max_mm/pixel 
-print('Resolution: ', Resolution)
+pixel = [s for s in array if "1" in s]
+
+if (len(pixel)) == 0:
+    print("Object is not found")
+else:
+    
+    Resolution = max_mm/len(pixel) 
+    print('Resolution: ', Resolution)
