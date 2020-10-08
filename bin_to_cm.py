4478d29a848bf4851f274e97160cae3d389be622
Алина Щербинина <48168698+alina-shcherbinina@users.noreply.github.com>
2020-10-08 18:28:12 +0800

    third attempt v1

--- bin_to_cm.py
+++ bin_to_cm.py
@@ -7,7 +7,7 @@ max_mm = float(text_file.readline()) # maximum mm first line
 lines = text_file.readlines()
 
 for i in range(1, len(lines)):
-    first = lines[i].split()
+    first = lines[i].split("\n")
     # print(first)
     array.extend(first)
 
@@ -17,4 +17,4 @@ if (len(pixel)) == 0:
     print("Object is not found")
 else:
     Resolution = max_mm/len(pixel) 
-    print('Resolution: ', Resolution)
+    print(max_mm, len(pixel),'Resolution: ', Resolution)
